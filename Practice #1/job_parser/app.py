c0eb3c05a9006160dde15fd2984841c8f7bef6be
work-smart-and-hard <koka092005@yandex.ru>
2024-07-04 23:34:29 +0300

    Added a design to the flask

--- Practice #1/job_parser/app.py
+++ Practice #1/job_parser/app.py
@@ -19,19 +19,12 @@ def fetch_vacancies():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     filters = {}
-    if request.method == 'POST':
-        # Получаем запрос из формы
-        search_query = request.form.get('search', '').strip()
-        
-        # Добавляем запрос в фильтры, если он существует
+    if request.method == 'POST': # Получаем запрос из формы
+        search_query = request.form.get('search', '').strip() # Добавляем запрос в фильтры, если он существует
         if search_query:
-            filters['name'] = search_query
-        
-        # Получаем дополнительные фильтры из формы
+            filters['name'] = search_query # Получаем дополнительные фильтры из формы
         filters.update({key: value for key, value in request.form.items() if key != 'search' and value})
-
-        # Получаем вакансии с учетом фильтров
-        vacancies = get_vacancies_with_filters(filters)
+        vacancies = get_vacancies_with_filters(filters) # Получаем вакансии с учетом фильтров
         return render_template('results.html', vacancies=vacancies, filters=filters)
     return render_template('results.html', vacancies=[], filters=filters)
 
